47ee992af7570a1b30bc6aa46387805f7cd27227
unknown <B413@s3-13.(none)>
2011-05-13 11:13:09 +0800

    quitFormDrag

--- plantsVSzombies/QuitGame.Designer.cs
+++ plantsVSzombies/QuitGame.Designer.cs
@@ -29,27 +29,14 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QuitGame));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lblExit = new System.Windows.Forms.Label();
             this.lblCancel = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-19, -49);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(437, 291);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // lblExit
             // 
             this.lblExit.Image = ((System.Drawing.Image)(resources.GetObject("lblExit.Image")));
-            this.lblExit.Location = new System.Drawing.Point(37, 176);
+            this.lblExit.Location = new System.Drawing.Point(55, 226);
             this.lblExit.Name = "lblExit";
             this.lblExit.Size = new System.Drawing.Size(160, 42);
             this.lblExit.TabIndex = 1;
@@ -58,7 +45,7 @@
             // lblCancel
             // 
             this.lblCancel.Image = ((System.Drawing.Image)(resources.GetObject("lblCancel.Image")));
-            this.lblCancel.Location = new System.Drawing.Point(213, 176);
+            this.lblCancel.Location = new System.Drawing.Point(232, 226);
             this.lblCancel.Name = "lblCancel";
             this.lblCancel.Size = new System.Drawing.Size(160, 42);
             this.lblCancel.TabIndex = 2;
@@ -68,24 +55,25 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(418, 243);
+            this.ClientSize = new System.Drawing.Size(431, 295);
             this.Controls.Add(this.lblCancel);
             this.Controls.Add(this.lblExit);
-            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "QuitGame";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "QuitGame";
             this.TransparencyKey = System.Drawing.SystemColors.Control;
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.QuitGame_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.QuitGame_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.QuitGame_MouseUp);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblExit;
         private System.Windows.Forms.Label lblCancel;
     }
--- plantsVSzombies/QuitGame.cs
+++ plantsVSzombies/QuitGame.cs
@@ -25,5 +25,41 @@ namespace plantsVSzombies
         {
             this.Close();
         }
+        #region//隐形窗体拖动
+        private bool isMouseDown = false;
+        private Point FormLocation;     //form的location
+        private Point mouseOffset;      //鼠标的按下位置
+
+        private void QuitGame_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown = true;
+                FormLocation = this.Location;
+                mouseOffset = Control.MousePosition;
+            }
+        }
+
+        private void QuitGame_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+        }
+
+        private void QuitGame_MouseMove(object sender, MouseEventArgs e)
+        {
+            int _x = 0;
+            int _y = 0;
+            if (isMouseDown)
+            {
+                Point pt = Control.MousePosition;
+                _x = mouseOffset.X - pt.X;
+                _y = mouseOffset.Y - pt.Y;
+
+                this.Location = new Point(FormLocation.X - _x, FormLocation.Y - _y);
+            }
+        }
+
+        #endregion
+
     }
 }
