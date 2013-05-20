a9811abc3a0e7d7eaaf5fc1e430653aafcac9162
dinglixiang <dlx_swpu@qq.com>
2013-05-20 21:12:58 +0800

    ka cao

--- plantsVSzombies/frmLevelOne.Designer.cs
+++ plantsVSzombies/frmLevelOne.Designer.cs
@@ -32,15 +32,21 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLevelOne));
             this.pictureBox_Peashooter = new System.Windows.Forms.PictureBox();
             this.timer_gameSpeed = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Peashooter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox_Peashooter
             // 
             this.pictureBox_Peashooter.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox_Peashooter.BackgroundImage")));
-            this.pictureBox_Peashooter.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox_Peashooter.Location = new System.Drawing.Point(99, 13);
             this.pictureBox_Peashooter.Name = "pictureBox_Peashooter";
-            this.pictureBox_Peashooter.Size = new System.Drawing.Size(99, 62);
+            this.pictureBox_Peashooter.Size = new System.Drawing.Size(70, 60);
             this.pictureBox_Peashooter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox_Peashooter.TabIndex = 0;
             this.pictureBox_Peashooter.TabStop = false;
@@ -50,6 +56,37 @@
             // 
             this.timer_gameSpeed.Tick += new System.EventHandler(this.timer_gameSpeed_Tick);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 88);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(89, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(375, 88);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.Location = new System.Drawing.Point(461, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(70, 72);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
             // frmLevelOne
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -57,6 +94,9 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(932, 680);
             this.Controls.Add(this.pictureBox_Peashooter);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -70,6 +110,9 @@
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmLevelOne_Paint);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmLevelOne_MouseMove);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Peashooter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,5 +122,8 @@
 
         private System.Windows.Forms.PictureBox pictureBox_Peashooter;
         private System.Windows.Forms.Timer timer_gameSpeed;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
